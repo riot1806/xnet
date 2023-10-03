d810d42c2217eabfc0c6172da104b18288b910d7
Bobur Hatamov <boburhatamov7@gmail.com>
2023-10-03 15:59:48 +0500

    create: nav and footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react'
+import s from './style.module.scss'
+
+const Footer:FC = () => {
+  return (
+    <div>Footer</div>
+  )
+}
+
+export default Footer
---
+++ src/components/Navbar/Nav.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react'
+import s from './styles.module.scss'
+
+const Nav:FC = () => {
+  return (
+    <>
+<h1 className={s.nav}>Nav</h1>
+    </>
+  )
+}
+
+export default Nav
